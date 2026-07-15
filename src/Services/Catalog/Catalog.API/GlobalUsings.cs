466d9e78678d6fbda9972fdf5ed3d01fedfe5e44
Fabricio Vieira <fabriciovieira.dev@gmail.com>
2026-07-14 22:52:00 -0300

    feat: get product pagination

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -8,3 +8,4 @@ global using Catalog.API.Data;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
+global using Marten.Pagination;
