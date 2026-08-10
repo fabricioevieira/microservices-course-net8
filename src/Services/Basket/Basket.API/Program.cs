1193aee982ed8c6139c2a7ec02ce007f64cc9cad
Fabricio Vieira <fabriciovieira.dev@gmail.com>
2026-08-09 22:18:02 -0300

    fix: Enable HTTPS for Discount gRPC client in Basket service
    Configured gRPC client to accept any server certificate for development by customizing HttpClientHandler in Program.cs. Updated docker-compose.override.yml to set GrpcSettings:DiscountUrl to https://discount.grpc:8081 and added discount.grpc as a service dependency.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -39,7 +39,15 @@ builder.Services.AddStackExchangeRedisCache(op =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-});
+}).ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        return handler;
+    });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
